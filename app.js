ec9432155fcbcd0cb9cf1604a9aea7cc44b1da47
Mazhar <mazharuddin1305@gmail.com>
2024-04-04 01:05:18 +0530

    new api key added

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.post("/",function(req,res){
   const url = "https://us22.api.mailchimp.com/3.0/lists/41c8c531eb"
   const Option = {
     method:"POST",
-    auth:"mazhar1305:8d80ffd35bb66b87b53fcadf9ae48e1f-us22"
+    auth:"mazhar1305:8965ffab13a7fc2940913926328cf661-us22"
   }
   
   const request = https.request(url,Option,function(response){
@@ -75,4 +75,10 @@ app.listen(process.env.PORT || 3000,function(){
 
 
 // 8d80ffd35bb66b87b53fcadf9ae48e1f-us22
+// 41c8c531eb.
+
+
+
+
+// 8965ffab13a7fc2940913926328cf661-us22
 // 41c8c531eb.
